ff90265738975c5ccba6d3a6509341ddb9732a3f
suvrat savran <suvratsavran@gmail.com>
2026-08-01 13:50:18 +0530

    done

---
+++ README.md
@@ -0,0 +1,22 @@
+#React 
+
+
+#parcel 
+- Devbuild
+- Localserver
+- HMR - hot module replacement - as soon you make the changes and save the file the reflection of changes will be seen to you on the browser without any refreshing .
+-It keeps the track of all the files and its statuses
+- File watching algorithm - Written in c++
+- Caching - Faster builds
+- Image Optimization
+- Minification of files size
+- Bundling 
+- Compress
+- content hashing
+- code splitting
+- tree shaking 
+- diagnostic 
+- error handling 
+- HTTPs
+- different dev and prod builds
+- differential bundling - supports older browser (type ="nomodule")
---
+++ app.js
@@ -0,0 +1,61 @@
+{/* <script>
+    const heading = document.createElement('h1');
+    heading.innerText = 'Hello, React! from javascript';
+    document.getElementById('root').appendChild(heading);
+   </script>  */}
+
+   {/*
+    <div id = "parent">
+        <div id = "child">
+            <h1>hello! from react</h1>
+        </div>
+    </div>
+    */}
+   
+import React from "react";
+import ReactDOM from "react-dom/client";
+// from where we are importing react and react-dom is from the node_modules folder which is created when we run npm install react react-dom
+
+   const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child1", key: "child1" },
+      [
+        React.createElement("h1", { key: "h1-1" }, "Hi I'm an h1 tag from react"),
+        React.createElement("h2", { key: "h2-1" }, "Hi I'm an h2 tag from react")
+      ]
+    ),
+
+    React.createElement(
+      "div",
+      { id: "child2", key: "child2" },
+      [
+        React.createElement("h1", { key: "h1-2" }, "Hi I'm an h1 tag from react"),
+        React.createElement("h2", { key: "h2-2" }, "Hi I'm an h2 tag from react")
+      ]
+    )
+  ]
+);
+
+
+
+    // const heading = React.createElement('h1',{
+    //     id:"heading",
+    //     abc:"def"},
+    //     "hello! from react");
+
+    // console.log(heading); 
+
+    // const root = ReactDOM.createRoot(document.getElementById('root'));
+    // root.render(heading);
+
+
+
+        console.log(parent); // this will print the object of the element created by react
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(parent);
+
